5599e7997e233b45e3af9a86eb743c28b6bf16e1
Naasor <amofasandra7@gmail.com>
2023-11-06 20:21:39 +0000

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,73 @@
 #include "lists.h"
 
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
 /**
- * is_palindrome - function that checks if a singly linked list is a palindrome
- * @head: pointer to the adress of the head node
- * Return: 1 if it is a palindrome, 0 otherwise
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *fst = *head;
-	int n = 0, i = 0, *arr = NULL; /* n referes to node keep in mind*/
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	if (*head == NULL || head == NULL || (*head)->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (fst)
+
+	tmp = *head;
+	while (tmp)
 	{
-		fst = fst->next;
-		n++;
+		size++;
+		tmp = tmp->next;
 	}
 
-	arr = malloc(sizeof(int) * n);
-	fst = *head;
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
 
-	while (fst)
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		arr[i++] = fst->n;
-		fst = fst->next;
-	}
-	for (i = 0; i < n / 2; i++)
-	{
-		if (arr[i] != arr[n - 1 - i])
-		{
-			free(arr);
+		if (tmp->n != rev->n)
 			return (0);
-		}
+		tmp = tmp->next;
+		rev = rev->next;
 	}
-	free(arr);
+	reverse_listint(&mid);
+
 	return (1);
 }
