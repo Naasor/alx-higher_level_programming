1bf60d83d005c9a086b2fbeb389c23685226c1d3
Naasor <amofasandra7@gmail.com>
2023-11-06 18:33:03 +0000

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if sentence == "":
         return (None)
     else:
         my_tuple = len(sentence), sentence[0]
