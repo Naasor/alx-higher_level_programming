9f6a13fbba683222f4fc6be48766296e5fddfc41
Naasor <amofasandra7@gmail.com>
2024-03-01 22:34:18 +0000

    pycodingstyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,21 +10,21 @@ from sys import argv
 def json_api():
     try:
         q = argv[1]
-        except Exception:
+    except Exception:
         q = ''
-        url = 'http://0.0.0.0:5000/search_user'
-        payload = {'q': q}
-        r = requests.post(url, payload)
-        try:
-            json = r.json()
-            if len(json) == 0:
-                print('No result')
-            else:
-                json_id = json.get('id')
-                json_name = json.get('name')
-                print('[{}] {}'.format(json_id, json_name))
-        except Exception:
-            print('Not a valid JSON')
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': q}
+    r = requests.post(url, payload)
+    try:
+        json = r.json()
+        if len(json) == 0:
+            print('No result')
+        else:
+            json_id = json.get('id')
+            json_name = json.get('name')
+            print('[{}] {}'.format(json_id, json_name))
+    except Exception:
+        print('Not a valid JSON')
 
 
 if __name__ == '__main__':
