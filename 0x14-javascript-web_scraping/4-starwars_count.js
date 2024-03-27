02b5bb6911d82ad87e7ccbd8d04011e959306d86
amofasandra7@gmail.com <amofasandra7@gmail.com>
2024-03-27 01:34:57 +0000

    fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
-const request = require('request')
-const apiUrl = process.argv[2]
 
-request.get(apiUrl, function (error, response, body) {
-  if (error) {
-    console.log(error)
-  } else {
-    let countWedge = 0
-    const films = JSON.parse(body).results
-    for (let i = 0; i < films.length; i++) {
-      const charList = films[i].characters
-      for (let j = 0; j < charList.length; j++) {
-        if (charList[j] === 'https://swapi.co/api/people/18/' || charList[j] === 'http://swapi.co/api/people/18/') {
-          countWedge += 1
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
         }
       }
     }
-    console.log(countWedge)
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-})
+});
