a6214b30e0aeecd7b42b0b5fc7f68948d5e599a7
Naasor <amofasandra7@gmail.com>
2024-03-27 08:41:11 -0700

    fixing semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
-const req = require('request')
-const id = process.argv[2]
+const req = require('request');
+const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
 req.get(url, (error, res, body) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, (error, res, body1) => {
-      if (error) {
-        console.log(error)
-      } else {
-      const data1 = JSON.parse(body1)
-      console.log(data1.name)
-      }
-    });
+    const data = JSON.parse(body);
+    const dd = data.characters;
+    for (const i of dd) {
+      req.get(i, (error, res, body1) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const data1 = JSON.parse(body1);
+          console.log(data1.name);
+        }
+      });
+    }
   }
-  }
-})
+});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const request = require('request')
-const url = process.argv[2]
+const request = require('request');
+const url = process.argv[2];
 request.get(url, (error, response, body) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
-    console.log(`code: ${response.statusCode}`)
+    console.log(`code: ${response.statusCode}`);
   }
-})
+});
